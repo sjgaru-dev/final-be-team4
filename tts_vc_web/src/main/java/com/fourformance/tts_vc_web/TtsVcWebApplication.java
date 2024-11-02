7bf330a4df74d8c787a6de9640d0e5ad631c88a6
SeungMin Oh <95595047+HiSeungmin@users.noreply.github.com>
2024-11-02 15:41:43 +0900

    Add files via upload

---
+++ tts_vc_web/src/main/java/com/fourformance/tts_vc_web/TtsVcWebApplication.java
@@ -0,0 +1,13 @@
+package com.fourformance.tts_vc_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtsVcWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtsVcWebApplication.class, args);
+	}
+
+}
---
+++ tts_vc_web/src/test/java/com/fourformance/tts_vc_web/TtsVcWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourformance.tts_vc_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtsVcWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
