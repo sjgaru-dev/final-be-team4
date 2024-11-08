4ca1733e6a18ceff5db071a6332ddbe60b6e0014
Luti <wns6619@gmail.com>
2024-11-08 11:57:26 +0900

    Remove .idea folder from Git tracking

---
+++ src/test/java/com/fourformance/tts_vc_web/domain/entity/ConcatStatusHistoryTest.java
@@ -0,0 +1,21 @@
+package com.fourformance.tts_vc_web.domain.entity;
+
+import com.fourformance.tts_vc_web.repository.ConcatStatusHistoryRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class ConcatStatusHistoryTest {
+
+    @Autowired
+    private ConcatStatusHistoryRepository ConcatStatusHistoryRepository;
+
+    @Test
+    public void
+
+}
