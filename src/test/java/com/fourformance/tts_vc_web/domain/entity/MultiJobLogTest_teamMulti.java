553787dfe65cf5c45fb8ebcd962a9ed23defb35a
yr0202 <yooram0202@naver.com>
2024-11-10 19:59:48 +0900

    Test: vc_detail test

---
+++ src/test/java/com/fourformance/tts_vc_web/domain/entity/MultiJobLogTest_teamMulti.java
@@ -0,0 +1,17 @@
+package com.fourformance.tts_vc_web.domain.entity;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+@ExtendWith(MockitoExtension.class)
+class MultiJobLogTest_teamMulti {
+  
+}
