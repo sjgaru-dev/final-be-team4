9f530f4b16b1d1131106260b24f5b1a2f33b5a65
Luti <wns6619@gmail.com>
2024-11-11 15:08:41 +0900

    fix : ..

--- src/test/java/com/fourformance/tts_vc_web/domain/entity/MemberTest.java
+++ src/test/java/com/fourformance/tts_vc_web/domain/entity/MemberTest.java
@@ -6,6 +6,7 @@ import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
