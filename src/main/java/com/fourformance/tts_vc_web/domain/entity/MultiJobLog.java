3f2b99e0a2fbf774c1e0b910800bab156f4691a8
yr0202 <yooram0202@naver.com>
2024-11-11 10:15:44 +0900

    Test: MultijobLog Entity modified

--- src/main/java/com/fourformance/tts_vc_web/domain/entity/MultiJobLog.java
+++ src/main/java/com/fourformance/tts_vc_web/domain/entity/MultiJobLog.java
@@ -40,37 +40,7 @@ public class MultiJobLog extends BaseEntity {
     private LocalDateTime endedAt;  // 컬럼명을 endedAt이라고 했지만 종료시점 보다는 API Status가 바뀌는 시점이라고 이해하면 좋습니다!
     // API 상태가 변경되면 이 객체의 endedAt을 now로 업데이트 시키고 새로운 객체에 새로운 상태를 담으면 됩니다!
 
-//    // 생성 메서드
-//    public static MultiJobLog createMultiJobLog(Project project, String projectName, ProjectType projectType,
-//                                                MultiJobLogStatusConst multiJobLogStatusConst, String failBy,
-//                                                String comment, Integer sequence) {
-//        MultiJobLog multiJobLog = new MultiJobLog();
-//        multiJobLog.project = project;
-//        multiJobLog.projectName = projectName;
-//        multiJobLog.projectType = projectType;
-//        multiJobLog.multiJobLogStatusConst = multiJobLogStatusConst;
-//        multiJobLog.failBy = failBy;
-//        multiJobLog.createdAt = LocalDateTime.now();
-//        multiJobLog.comment = comment;
-//        multiJobLog.sequence = sequence;
-//        return multiJobLog;
-//    }
 
-//    // 생성 메서드
-//    public static MultiJobLog createMultiJobLog(Project project,
-//                                                MultiJobLogStatusConst multiJobLogStatusConst, String failBy,
-//                                                String comment, Integer sequence) {
-//        MultiJobLog multiJobLog = new MultiJobLog();
-//        multiJobLog.project = project;
-//        multiJobLog.projectName = project.getProjectName(); // Project에서 이름 가져오기
-//        multiJobLog.projectType = ProjectType.valueOf(project.getClass().getSimpleName().toUpperCase()); // Project에서 타입 가져오기
-//        multiJobLog.multiJobLogStatusConst = multiJobLogStatusConst;
-//        multiJobLog.failBy = failBy;
-//        multiJobLog.createdAt = LocalDateTime.now();
-//        multiJobLog.comment = comment;
-//        multiJobLog.sequence = sequence;
-//        return multiJobLog;
-//    }
 public static MultiJobLog createMultiJobLog(Project project, MultiJobLogStatusConst multiJobLogStatusConst,
                                             String failBy, String comment, Integer sequence) {
     MultiJobLog multiJobLog = new MultiJobLog();
