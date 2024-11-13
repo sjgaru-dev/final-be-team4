eb19b851fbcda88063b17d66999e4a519071b2cf
Luti <wns6619@gmail.com>
2024-11-13 16:14:26 +0900

    feat: 베이스 리스폰스 디티오 및 커스텀 에러코드 적용

---
+++ src/main/java/com/fourformance/tts_vc_web/common/exception/common/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.fourformance.tts_vc_web.common.exception.common;
+
+import com.fourformance.tts_vc_web.dto.response.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // **
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponseDto> handleBusinessException(BusinessException e) {
+        // ErrorResponseDto 생성
+        ErrorResponseDto errorResponse = ErrorResponseDto.of(e.getErrorCode());
+
+        return new ResponseEntity<>(errorResponse, e.getErrorCode().getHttpStatus());
+    }
+
+
+    // 기타 모든 예외에 대한 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleException(Exception e) {
+        ErrorResponseDto errorResponse = ErrorResponseDto.of(ErrorCode.UNKNOWN_ERROR);
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
