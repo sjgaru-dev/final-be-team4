a09fd1c0f3903c33ef75684e72d9c7c4f6d3684b
Luti <wns6619@gmail.com>
2024-11-13 16:14:26 +0900

    feat: 베이스 리스폰스 디티오 및 커스텀 에러코드 적용

---
+++ src/main/java/com/fourformance/tts_vc_web/dto/response/ErrorResponseDto.java
@@ -0,0 +1,16 @@
+package com.fourformance.tts_vc_web.dto.response;
+
+import com.fourformance.tts_vc_web.common.exception.common.ErrorCode;
+
+public class ErrorResponseDto extends ResponseDto {
+
+    private ErrorResponseDto(ErrorCode errorCode) {
+        super(false, errorCode.getCode(), errorCode.getMessage());
+    }
+
+    public static ErrorResponseDto of(ErrorCode errorCode) {
+        return new ErrorResponseDto(errorCode);
+    }
+
+
+}
