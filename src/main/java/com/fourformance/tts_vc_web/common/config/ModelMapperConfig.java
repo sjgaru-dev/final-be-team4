33ccbc9fa12d427dc7ef99a61c7608f7d21fef88
yr0202 <yooram0202@naver.com>
2024-11-14 21:57:23 +0900

    Env: ModelMapperConfig file add

---
+++ src/main/java/com/fourformance/tts_vc_web/common/config/ModelMapperConfig.java
@@ -0,0 +1,17 @@
+package com.fourformance.tts_vc_web.common.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setFieldMatchingEnabled(true);
+        return modelMapper;
+    }
+}
