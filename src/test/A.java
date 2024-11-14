422122a8ecf5fc0d8df8da75b58ef74b5c73713a
sojungCho <belldorsy@gmail.com>
2024-11-14 13:37:39 +0900

    Feat:: S3  total download

--- src/test/A.java
+++
@@ -1,5 +0,0 @@
-/Users/luti/Desktop/final-be-team4/src/main/java/com/fourformance/tts_vc_web/service/common/S3Service.java:267:62
-java: method createMemberAudioMeta in class com.fourformance.tts_vc_web.domain.entity.MemberAudioMeta cannot be applied to given types;
-  required: com.fourformance.tts_vc_web.domain.entity.Member,java.lang.String,java.lang.String,com.fourformance.tts_vc_web.common.constant.AudioType
-  found:    com.fourformance.tts_vc_web.domain.entity.Member,java.lang.String,com.fourformance.tts_vc_web.common.constant.AudioType
-  reason: actual and formal argument lists differ in length
