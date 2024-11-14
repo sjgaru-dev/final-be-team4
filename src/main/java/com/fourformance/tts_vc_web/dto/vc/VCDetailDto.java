40da6b406037f8b85f8b2a2945739bfef0dbea48
HiSeungmin <osminosmin7180@gmail.com>
2024-11-14 16:03:45 +0900

    Feat: VC 컨트롤러, 서비스, Dto 생성

--- src/main/java/com/fourformance/tts_vc_web/dto/vc/VCDetailDto.java
+++ src/main/java/com/fourformance/tts_vc_web/dto/vc/VCDetailDto.java
@@ -1,4 +1,38 @@
 package com.fourformance.tts_vc_web.dto.vc;
 
+import com.fourformance.tts_vc_web.domain.entity.VCDetail;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class VCDetailDto {
+
+    private Long id; // 상세 정보 ID
+    private Long projectId; // 프로젝트 ID
+    private Long memberAudioMetaId; // MemberAudioMeta ID
+    private Boolean isChecked; // 체크 여부
+    private String unitScript; // 단위 스크립트
+    private Boolean isDeleted; // 삭제 여부
+
+    private static ModelMapper modelMapper = new ModelMapper();
+
+    // VCDetailDto -> VCDetail 매핑 메서드
+    public VCDetail createVCDetail() {
+        modelMapper.getConfiguration()
+                .setFieldAccessLevel(Configuration.AccessLevel.PRIVATE)
+                .setFieldMatchingEnabled(true);
+        return modelMapper.map(this, VCDetail.class);
+    }
+
+    // VCDetail -> VCDetailDto 매핑 메서드
+    public static VCDetailDto createVCDetailDto(VCDetail vcDetail) {
+        return modelMapper.map(vcDetail, VCDetailDto.class);
+    }
 }
