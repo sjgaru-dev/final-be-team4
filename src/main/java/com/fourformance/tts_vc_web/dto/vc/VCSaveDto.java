ef09b83fe0b1b718aaa9b479bde22a9c52d751d6
yr0202 <yooram0202@naver.com>
2024-11-18 21:55:23 +0900

    Feat: VC save feature 수정중

--- src/main/java/com/fourformance/tts_vc_web/dto/vc/VCSaveDto.java
+++ src/main/java/com/fourformance/tts_vc_web/dto/vc/VCSaveDto.java
@@ -1,20 +1,29 @@
 package com.fourformance.tts_vc_web.dto.vc;
 
+import com.fourformance.tts_vc_web.common.constant.APIStatusConst;
+import com.fourformance.tts_vc_web.domain.entity.MemberAudioMeta;
+import com.fourformance.tts_vc_web.domain.entity.MemberAudioVC;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class VCSaveDto {
-    /***
-     *  프로젝트 id
-     *  apiStatus
-     *  MemberAudioVC : vc_src 일대다 관계
-     *  member_audio_meta : vc_trg 일대일 관계 -> bucket router
-     *
-     */
+
+    // VCProject 관련 필드
+    private Long projectId;
+    private String projectName;
+//    private APIStatusConst apiStatus; // 이건 로드 기능 같은데..
+    private MemberAudioMeta memberTargetAudioMeta; // trg audio
+    // private String trgVoiceId; // 깃에서 풀 받아오면 주석 풀기
+
+    // VCDetail 관련 필드
+    List<VCSaveDetailDto> vcDetails;
+
 }
