ed747cb6cb202efa50a660ff884cd80b8f04795c
Luti <wns6619@gmail.com>
2024-11-19 18:13:57 +0900

    fix: 더미 생성기 수정 (tts 디테일이 보이스 스타일을 참조하도록)

--- src/main/java/com/fourformance/tts_vc_web/repository/ProjectRepository.java
+++ src/main/java/com/fourformance/tts_vc_web/repository/ProjectRepository.java
@@ -1,19 +1,13 @@
 package com.fourformance.tts_vc_web.repository;
 
 import com.fourformance.tts_vc_web.domain.entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
-    // sojeong-작성,
-    // 사용처 workspace의 최근작업내역 탑10만들기
-    // ProjectService에서 메서드 사용
-    @Query()
-
-    List<Project> findTop10ByMemberIdOrderByCreatedAtDesc(Long memberId);
+    // (워크스페이스 - 홈 화면) 멤버 id에 해당하는 최근 프로젝트 5개 조회하는 메서드 - 이의준, 조소정
+    List<Project> findTop5ByMemberIdOrderByCreatedAtDesc(Long memberId);
 }
--- src/main/java/com/fourformance/tts_vc_web/service/workspace/ProjectService.java
+++ src/main/java/com/fourformance/tts_vc_web/service/workspace/ProjectService.java
@@ -1,47 +1,43 @@
-package com.fourformance.tts_vc_web.service.workspace;
-
-import com.fourformance.tts_vc_web.common.exception.common.ErrorCode;
-import com.fourformance.tts_vc_web.domain.entity.Project;
-import com.fourformance.tts_vc_web.dto.response.DataResponseDto;
-import com.fourformance.tts_vc_web.dto.response.ErrorResponseDto;
-import com.fourformance.tts_vc_web.dto.response.ResponseDto;
-import com.fourformance.tts_vc_web.dto.workspace.ProjectDto;
-import com.fourformance.tts_vc_web.repository.ProjectRepository;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Service
-public class ProjectService {
-
-    private final ProjectRepository projectRepository;
-
-    public ProjectService(ProjectRepository projectRepository) {
-        this.projectRepository = projectRepository;
-    }
-
-    public ResponseDto getRecentProjects(Long memberId) {
-        // memberId가 null이면 ErrorResponseDto 반환
-        if (memberId == null) {
-            return ErrorResponseDto.of(ErrorCode.MEMBER_NOT_FOUND);
-        }
-
-        List<Project> projects = projectRepository.findTop10ByMemberIdOrderByCreatedAtDesc(memberId);
-
-        List<ProjectDto> projectDtoList = new ArrayList<>();
-        for (Project project : projects) {
-            String type = project.getClass().getSimpleName(); // 클래스 이름으로 타입 설정
-            projectDtoList.add(new ProjectDto(
-                    project.getId(),
-                    "/projects/" + project.getId(),
-                    project.getProjectName(),
-                    type, // DTO에 타입 추가
-                    project.getCreatedAt(),
-                    project.getUpdatedAt()
-            ));
-        }
-        return DataResponseDto.of(projectDtoList); // 리스트 반환
-    }
-}
+//package com.fourformance.tts_vc_web.service.workspace;
+//
+//import com.fourformance.tts_vc_web.common.exception.common.BusinessException;
+//import com.fourformance.tts_vc_web.common.exception.common.ErrorCode;
+//import com.fourformance.tts_vc_web.domain.entity.Project;
+//import com.fourformance.tts_vc_web.dto.response.DataResponseDto;
+//import com.fourformance.tts_vc_web.dto.response.ResponseDto;
+//import com.fourformance.tts_vc_web.dto.workspace.RecentProjectDto;
+//import com.fourformance.tts_vc_web.repository.ProjectRepository;
+//import java.util.ArrayList;
+//import java.util.List;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class ProjectService {
+//
+//    private final ProjectRepository projectRepository;
+//
+//    public ResponseDto getRecentProjects(Long memberId) {
+//        // memberId가 null이면 ErrorResponseDto 반환
+//        if (memberId == null) {
+//            throw new BusinessException(ErrorCode.MEMBER_NOT_FOUND);
+//        }
+//
+//        List<Project> projects = projectRepository.findTop5ByMemberIdOrderByCreatedAtDesc(memberId);
+//
+//        List<RecentProjectDto> projectDtoList = new ArrayList<>();
+//        for (Project project : projects) {
+//            String type = project.getClass().getSimpleName(); // 클래스 이름으로 타입 설정
+//            projectDtoList.add(new RecentProjectDto(
+//                    project.getId(),
+//                    "/projects/" + project.getId(),
+//                    project.getProjectName(),
+//                    type, // DTO에 타입 추가
+//                    project.getCreatedAt(),
+//                    project.getUpdatedAt()
+//            ));
+//        }
+//        return DataResponseDto.of(projectDtoList); // 리스트 반환
+//    }
+//}
