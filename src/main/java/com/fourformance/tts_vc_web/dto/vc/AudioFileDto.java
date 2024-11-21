4f7880779466b06de557ef529ca435e64b6df15e
yr0202 <yooram0202@naver.com>
2024-11-21 14:11:01 +0900

    Fix: vc save modified

--- src/main/java/com/fourformance/tts_vc_web/dto/vc/AudioFileDto.java
+++ src/main/java/com/fourformance/tts_vc_web/dto/vc/AudioFileDto.java
@@ -15,6 +15,7 @@ public class AudioFileDto {
     private Long s3MemberAudioMetaId; //  S3 메타 정보 (DB에서 조회)
     private String localFileName; // 로컬 업로드 파일의 이름 (MultipartFile 매칭)
     private String unitScript; // 선택 사항: 텍스트 스크립트 (nullable)
+    private Boolean isChecked; // 체크 표시 여부
 
     // 유효성 검사 메서드
     public void validate() {
