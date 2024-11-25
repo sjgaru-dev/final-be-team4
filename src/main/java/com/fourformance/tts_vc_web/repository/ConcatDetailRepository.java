1abbdbe279f556348ce4cb9591ea267a4fb1bf45
Luti <wns6619@gmail.com>
2024-11-25 10:32:09 +0900

    fix: ...

--- src/main/java/com/fourformance/tts_vc_web/repository/ConcatDetailRepository.java
+++ src/main/java/com/fourformance/tts_vc_web/repository/ConcatDetailRepository.java
@@ -1,15 +1,23 @@
 package com.fourformance.tts_vc_web.repository;
 
 import com.fourformance.tts_vc_web.domain.entity.ConcatDetail;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ConcatDetailRepository extends JpaRepository<ConcatDetail, Long> {
+
+    // Concat 프로젝트의 id로 Concat 디테일 리스트 조회 - 의준
+    List<ConcatDetail> findByConcatProject_Id(Long projectId);
+
+    // Concat Detail Id가 담긴 List로 ConcatDetail 객체 반환 받기 - 의준
+//    @Query("SELECT t FROM ConcatDetail c WHERE c.id IN :concatDetailIdList")
+    List<ConcatDetail> findByIdIn(List<Long> concatDetailIds);
+//    List<ConcatDetail> findByConcatDetailIds(@Param("concatDetailIdList") List<Long> concatDetailIdList);
+
     // 특정 ConcatProject와 연관된 모든 ConcatDetail 조회
     List<ConcatDetail> findByConcatProjectId(Long projectId);
 
--- src/main/java/com/fourformance/tts_vc_web/repository/OutputAudioMetaRepository.java
+++ src/main/java/com/fourformance/tts_vc_web/repository/OutputAudioMetaRepository.java
@@ -1,33 +1,52 @@
 package com.fourformance.tts_vc_web.repository;
 
 import com.fourformance.tts_vc_web.domain.entity.OutputAudioMeta;
-import com.fourformance.tts_vc_web.dto.workspace.RecentExportDto;
-import org.springframework.data.jpa.repository.EntityGraph;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 @Repository
 public interface OutputAudioMetaRepository extends JpaRepository<OutputAudioMeta, Long> {
 
 
     @Query(""" 
-    SELECT o
-    FROM OutputAudioMeta o
-    LEFT JOIN o.ttsDetail t
-    LEFT JOIN t.ttsProject tp
-    LEFT JOIN o.vcDetail v
-    LEFT JOIN v.vcProject vp
-    LEFT JOIN o.concatProject c
-    WHERE (tp.member.id = :memberId AND tp IS NOT NULL)
-       OR (vp.member.id = :memberId AND vp IS NOT NULL)
-       OR (c.member.id = :memberId AND c IS NOT NULL)
-    ORDER BY o.createdAt DESC
-    """)
+            SELECT o
+            FROM OutputAudioMeta o
+            LEFT JOIN o.ttsDetail t
+            LEFT JOIN t.ttsProject tp
+            LEFT JOIN o.vcDetail v
+            LEFT JOIN v.vcProject vp
+            LEFT JOIN o.concatProject c
+            WHERE (tp.member.id = :memberId AND tp IS NOT NULL)
+               OR (vp.member.id = :memberId AND vp IS NOT NULL)
+               OR (c.member.id = :memberId AND c IS NOT NULL)
+            ORDER BY o.createdAt DESC
+            """)
     List<OutputAudioMeta> findTop5ByMemberId(@Param("memberId") Long memberId);
 
+    // 최근 생성된 5개의 OutputAudioMeta 조회 (삭제되지 않은 데이터만)
+    @Query("SELECT o FROM OutputAudioMeta o " +
+            "WHERE o.isDeleted = false " +
+            "ORDER BY o.createdAt DESC")
+    List<OutputAudioMeta> findTop5RecentOutputAudioMeta();
+
+    // TTS Detail Id로 생성된 오디오들을 찾아 리스트로 반환 - 승민
+    @Query("SELECT o FROM OutputAudioMeta o WHERE o.ttsDetail.id IN :ttsDetailIds AND o.isDeleted = false")
+    List<OutputAudioMeta> findByTtsDetailAndIsDeletedFalse(@Param("ttsDetailIds") List<Long> ttsDetailIds);
+
+    // VC Detail Id로 생성된 오디오들을 찾아 리스트로 반환 - 승민
+    @Query("SELECT o FROM OutputAudioMeta o WHERE o.vcDetail.id = :vcDetailId AND o.isDeleted = false")
+    List<OutputAudioMeta> findAudioUrlsByVcDetail(@Param("vcDetailId") Long vcDetailId);
+
+    // TTS Detail Id로 생성된 오디오들을 찾아 리스트로 반환 - 승민
+    @Query("SELECT o FROM OutputAudioMeta o WHERE o.vcDetail.id IN :vcDetailIds AND o.isDeleted = false")
+    List<OutputAudioMeta> findByVcDetailAndIsDeletedFalse(@Param("vcDetailIds") List<Long> vcDetailIds);
+
+    // Concat Project Id로 생성된 오디오를 찾아 리스트로 변환 - 의준
+    @Query("SELECT o FROM OutputAudioMeta o WHERE o.concatProject.id = :concatProjectId AND o.isDeleted = false")
+    List<OutputAudioMeta> findAudioUrlsByConcatProject(@Param("concatProjectId") Long concatProjectId);
 
 
 }
