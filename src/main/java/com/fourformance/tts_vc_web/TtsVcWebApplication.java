be4fb22a3227ab76581a12b2d1ef9cfd94c28230
Luti <wns6619@gmail.com>
2024-11-26 13:36:57 +0900

    feat: querydsl 설정

--- src/main/java/com/fourformance/tts_vc_web/TtsVcWebApplication.java
+++ src/main/java/com/fourformance/tts_vc_web/TtsVcWebApplication.java
@@ -1,7 +1,10 @@
 package com.fourformance.tts_vc_web;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TtsVcWebApplication {
@@ -9,5 +12,10 @@ public class TtsVcWebApplication {
     public static void main(String[] args) {
         System.out.println("GOOGLE_APPLICATION_CREDENTIALS: " + System.getenv("GOOGLE_APPLICATION_CREDENTIALS"));
         SpringApplication.run(TtsVcWebApplication.class, args);
+    }
+
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
     }
 }
