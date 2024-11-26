4f104a54d3a8c742cb4f7706dd2ca995cb4ae4c6
jeongwonwoo <zkjjx@naver.com>
2024-11-26 16:34:19 +0900

    회원 작업 진행중

--- src/main/java/com/fourformance/tts_vc_web/dto/member/MemberUpdateRequestDto.java
+++ src/main/java/com/fourformance/tts_vc_web/dto/member/MemberUpdateRequestDto.java
@@ -2,6 +2,7 @@ package com.fourformance.tts_vc_web.dto.member;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Getter
@@ -18,5 +19,6 @@ public class MemberUpdateRequestDto {
     private Long memberId; // 수정할 회원의 고유 ID
     private String pwd; // 수정할 비밀번호
     private String phoneNumber; // 수정할 연락처
-    private Date birthDate; // 수정할 생년월일
+    private String name; // 수정할 이름
+
 }
