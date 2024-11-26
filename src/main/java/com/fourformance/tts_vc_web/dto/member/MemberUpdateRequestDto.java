2ccd7abd332f5e7234f48e75cd73647fdbeb6e27
HiSeungmin <osminosmin7180@gmail.com>
2024-11-26 15:03:03 +0900

    Fix: fixed brnach

---
+++ src/main/java/com/fourformance/tts_vc_web/dto/member/MemberUpdateRequestDto.java
@@ -0,0 +1,22 @@
+package com.fourformance.tts_vc_web.dto.member;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MemberUpdateRequestDto {
+    /**
+     * 회원 정보 수정 요청 DTO
+     * 사용자가 수정할 정보를 서버로 전달할 때 사용됩니다.
+     */
+
+    private Long memberId; // 수정할 회원의 고유 ID
+    private String pwd; // 수정할 비밀번호
+    private String phoneNumber; // 수정할 연락처
+    private Date birthDate; // 수정할 생년월일
+}
