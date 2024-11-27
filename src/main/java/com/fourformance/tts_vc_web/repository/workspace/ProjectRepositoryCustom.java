918bf64292a44f90bdab07a3ecc6278b6e7a4600
sojungCho <belldorsy@gmail.com>
2024-11-27 10:27:36 +0900

    ãSAVE : new sojeong

--- src/main/java/com/fourformance/tts_vc_web/repository/workspace/ProjectRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-package com.fourformance.tts_vc_web.repository.workspace;
-
-import com.fourformance.tts_vc_web.dto.workspace.ProjectListDto;
-import java.util.List;
-
-public interface ProjectRepositoryCustom {
-    List<ProjectListDto> findProjectsBySearchCriteria(Long memberId, String keyword);
-}
